21982a6075612c4a97bc37da8eccd9bafd34477b
MateusPitura <mateuspitura@gmail.com>
2024-09-07 19:22:09 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/olhoseco/main.java
@@ -0,0 +1,9 @@
+package com.mycompany.olhoseco;
+
+public class main {
+
+    public static void main(String[] args) {
+        OlhoSeco mainView = new OlhoSeco();
+        mainView.setVisible(true);
+    }
+}
